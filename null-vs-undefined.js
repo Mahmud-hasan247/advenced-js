85c49df8338d9a1c9114cabcd6f4e3bfde7d50e8
Mahmud-hasan247 <mahmudhasan5406@gmail.com>
2020-08-08 02:27:58 +0600

    null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+var name;
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+function add(num1, num2){
+    console.log( num2);
+}
+const result = add( 5);
+console.log(result);
+function add(num1, num2){
+    console.log(num1 + num2)
+    return
+}
+const result = add(35, 45);
+console.log(result);
+
+const defined = undefined;
+console.log(defined);
