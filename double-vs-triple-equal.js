ebe671293863c40a03b3a7d931cc1ba18c07e747
Mahmud-hasan247 <mahmudhasan5406@gmail.com>
2020-08-08 02:52:59 +0600

    double-vs-triple-equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,15 @@
+// const first = 3;
+// const second = 3;
+// const first = 3;
+// const second = "3";
+// const first = 3;
+// const second = "3";
+const first = 0;
+const second = false;
+
+if (first === second ){
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
