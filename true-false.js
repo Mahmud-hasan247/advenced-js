a4efa394b36dfe16c1a96abb93d2ea612c8d3c50
Mahmud-hasan247 <mahmudhasan5406@gmail.com>
2020-08-07 22:49:53 +0600

    True-False

---
+++ true-false.js
@@ -0,0 +1,21 @@
+// Falsy:-
+// const age = 0;
+// const name = "";
+// const name = null;
+// const name = NaN;
+// let name;
+
+// Truthy:-
+// const age = 33;
+// const name = "mahmud hasan";
+// const name = " ";
+// const name = "0";
+
+console.log(name);
+
+if (name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
